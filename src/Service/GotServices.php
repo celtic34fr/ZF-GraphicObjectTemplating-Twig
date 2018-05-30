152991fbe9916f11dd535307d32b8ccc83b4d6de
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2018-05-30 17:36:07 +0200

    ajout début méthode GotServices

--- src/Service/GotServices.php
+++ src/Service/GotServices.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/05/18
- * Time: 12:59
- */
 
 namespace GraphicObjectTemplating\Service;
 
+use GraphicObjectTemplating\OObjects\OObject;
+use GraphicObjectTemplating\OObjects\OSContainer;
+use GraphicObjectTemplating\OObjects\OESContainer;
+use Zend\View\Model\JsonModel;
+use Zend\View\Model\ViewModel;
 
 class GotServices
 {
+    private $_serviceManager;
+    private $_twigRender;
+
+    public function __construct($sm, $tr)
+    {
+        $this->_serviceManager = $sm;
+        $this->_twigRender = $tr;
+        return $this;
+    }
 
 }
