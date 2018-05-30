35cbf87cbe4d4aa703a4541a6bff3e3a090c208f
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2018-05-30 23:08:49 +0200

    finalisation codage GotRender

--- src/View/Helper/GotRender.php
+++ src/View/Helper/GotRender.php
@@ -1,15 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/05/18
- * Time: 12:57
- */
 
 namespace GraphicObjectTemplating\View\Helper;
 
+use GraphicObjectTemplating\Service\GotServices;
+use Zend\View\Helper\AbstractHelper;
 
-class GotRender
+class GotRender extends AbstractHelper
 {
+    /** @var GotServices $gs */
+    protected $gs;
 
+    public function __construct($gs)
+    {
+        $this->gs = $gs;
+        return $this;
+    }
+
+    public function __invoke($object)
+    {
+        $html = $this->gs->render($object);
+        return $html;
+    }
 }
+?>
