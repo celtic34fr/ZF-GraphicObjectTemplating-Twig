00d6f3544e039fe042e872f2258a28795f3bc69c
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2018-05-31 09:28:41 +0200

    ajout header

--- src/Service/GotServices.php
+++ src/Service/GotServices.php
@@ -29,6 +29,7 @@ class GotServices
         return $this;
     }
 
+    /** rendu de l'objet en code HTML */
     public function render($object)
     {
             if ($object != null) {
@@ -66,5 +67,85 @@ class GotServices
             }
             return false;
         }
+    }
+
+    /** récupération et formatage des enttêtes chargement ressources CSS & Js */
+    public function header($object)
+    {
+        $view = new ViewModel();
+
+        $scripts = $this->rscs($objets); // méthode dde récupération filtrage des ressources
+
+        if ($scripts) {
+            $view->setTemplate('graphic-object-templating/got/got-header.twig');
+            $view->setVariable('scripts', array('css' => $scripts['cssScripts'], 'js' => $scripts['jsScripts']));
+            $renduHtml = $this->_twigRender->render($view);
+            return $renduHtml;
+        }
+        return false;
+    }
+
+    public function rscs($objects)
+    {
+        if ($objects != null) {
+            if (!($objects instanceof OObject)) {
+                $objects = OObject::buildObject($objects);
+            }
+            $cssScripts = $jsScripts = [];
+            $rscs = "";
+            if (!is_array($objects)) {
+                $objects = array(0 => $objects);
+            }
+
+            foreach ($objects as $object) {
+                if ($object != null) {
+                    $properties = $object->getProperties();
+                    $prefix = 'graphicobjecttemplating/oobjects/';
+                    if (array_key_exists('extension', $properties) && $properties['extension']) {
+                        $prefix = $properties['resources']['prefix'];
+                    }
+                    $rscs = (isset($properties['resources'])) ? $properties['resources'] : "";
+                    if (!empty($rscs) && ($rscs !== false)) {
+                        $cssList = $rscs['css'];
+                        $jsList = $rscs['js'];
+                        if (!empty($cssList)) {
+                            foreach ($cssList as $item) {
+                                if (!in_array($item, $cssScripts)) {
+                                    $cssScripts[] = $prefix . $properties['typeObj'] . '/' . $properties['object'] . '/' . $item;
+                                }
+                            }
+                        }
+                        if (!empty($jsList)) {
+                            foreach ($jsList as $item) {
+                                if (!in_array($item, $jsScripts)) {
+                                    $jsScripts[] = $prefix . $properties['typeObj'] . '/' . $properties['object'] . '/' . $item;
+                                }
+                            }
+                        }
+                    }
+
+                    if ($object instanceof OSContainer || $object instanceof OESContainer) {
+                        $children = $object->getChildren();
+                        if (!empty($children)) {
+                            foreach ($children as $child) {
+                                $tmpArray = $this->headerChild($child);
+                                foreach ($tmpArray['css'] as $css) {
+                                    if (!in_array($css, $cssScripts)) {
+                                        $cssScripts[] = $css;
+                                    }
+                                }
+                                foreach ($tmpArray['js'] as $js) {
+                                    if (!in_array($js, $jsScripts)) {
+                                        $jsScripts[] = $js;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return ['cssScripts' => $cssScripts, 'jsScripts' => $jsScripts];
+        }
+        return false;
     }
 }
--- src/View/Helper/GotHeader.php
+++ src/View/Helper/GotHeader.php
@@ -1,15 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 29/05/18
- * Time: 12:57
- */
 
 namespace GraphicObjectTemplating\View\Helper;
 
+use GraphicObjectTemplating\Service\GotServices;
+use Zend\View\Helper\AbstractHelper;
 
-class GotHeader
+class GotHeader extends AbstractHelper
 {
+    /** @var GotServices $gs */
+    protected $gs;
 
+    public function __construct($gs)
+    {
+        $this->gs = $gs;
+        return $this;
+    }
+
+    public function __invoke($object)
+    {
+        $html = $this->gs->header($object);
+        return $html;
+    }
 }
+?>
