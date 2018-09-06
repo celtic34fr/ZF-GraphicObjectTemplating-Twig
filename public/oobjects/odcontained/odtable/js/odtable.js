7655308f63552db8159828932f5a10be2d45d374
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2018-09-06 22:30:16 +0200

    ODTable : rédaction méthode JavaScript de mise à jour suite à suppression de ligne dans un tableau

--- public/oobjects/odcontained/odtable/js/odtable.js
+++ public/oobjects/odcontained/odtable/js/odtable.js
@@ -10,3 +10,11 @@ odtable.prototype = {
 
     },
 };
+
+function rmLineUpdate(noLine, maxLine) {
+    for (var idx = (noLine + 1); idx <= (maxLine + 1); idx++) {
+        $("#tabListParamsG7PP .lno"+idx).data("lno", (idx - 1));
+        $("#tabListParamsG7PP .lno"+idx+" .cno1").html(idx - 1);
+        $("#tabListParamsG7PP .lno"+idx).removeClass("lno"+idx).addClass("lno"+(idx-1));
+    };
+}
