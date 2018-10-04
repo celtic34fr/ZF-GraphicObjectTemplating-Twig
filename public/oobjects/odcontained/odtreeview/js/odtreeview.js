c88ee0ea4c4398b41d9e13ddb33d024a1754a6b5
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2018-10-04 18:01:24 +0200

    ODTreeview : ajout gestion évènement et récupération des valeurs de feuille/noeud sélectionnés suppression de ressources inutiles

--- public/oobjects/odcontained/odtreeview/js/odtreeview.js
+++ public/oobjects/odcontained/odtreeview/js/odtreeview.js
@@ -1,12 +1,21 @@
 function odtreeview(obj) {
     this.id = obj.attr('id');
+    this.form   = obj.data('form');
 }
 
 odtreeview.prototype = {
     getData: function (evt) {
-
+        let selected = '';
+        $('#'+this.id+' li.selected').each(function () {
+            selected += $(this).data('lvl') + '.' + $(this).data('ord') + "-"
+        });
+        selected = selected.substring(0, selected.length - 1);
+        let chps = "id=" + this.id + "&value='" + selected + "'&event='click'";
+        return chps;
     },
     setData: function (data) {
-
+        $.each(data, function (i, value) {
+            $('#'+this.id+' li[data-lvl="'+ value.lvl +'"][data-ord="'+ value.ord +'"]').addClass('selected');
+        });
     },
 };
