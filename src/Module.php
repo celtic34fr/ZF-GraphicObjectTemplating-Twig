20d9d9c3e65282969914d5fd08800b7427d67a05
celtic34fr <gilbert.armengaud@gmail.com>
2019-04-03 19:07:05 +0200

    GOT Moudule : prise en compte pas de paramètres de session + objet persistant codé en paamètre

--- src/Module.php
+++ src/Module.php
@@ -2,10 +2,13 @@
 
 namespace GraphicObjectTemplating;
 
+use GraphicObjectTemplating\Factory\PersistObjectInterface;
+use GraphicObjectTemplating\OObjects\OObject;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
 use Zend\Mvc\MvcEvent;
+use Zend\ServiceManager\ServiceManager;
 use Zend\Session\Config\SessionConfig;
 use Zend\Session\Container;
 use Zend\Session\SessionManager;
@@ -13,12 +16,30 @@ use Zend\Session\SessionManager;
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
     ViewHelperProviderInterface
 {
+    /**
+     * @param MvcEvent $e
+     */
     public function onBootstrap(MvcEvent $e)
     {
-        $config = $e->getApplication()->getServiceManager()->get('Config');
-        $paramsSession = $config['gotParameters']['sessionParms'];
-        if (!empty($paramsSession)) {
+        $serviceManager = $e->getApplication()->getServiceManager();
+        $config = $serviceManager->get('Config');
+        $gotParams = $config['gotParameters'] ?? [];
+        if (!empty($gotParams)) {
+            $paramsSession = $gotParams['sessionParms'] ?? [];
+            $persistantObjects = $gotParams["persistantObjects"] ?? [];
+            if (empty($paramsSession)) {
+                $paramsSession = [
+                    'remember_me_seconds' => 3600, // 2 heures de temps de vie (rememberMe)
+                    'gc_maxlifetime' => 14400,     // 4 heures de temps de vie max
+                    'gc_divisor' => 1,
+                    'use_cookies' => true,
+                    'cookie_httponly' => true,
+                ];
+            }
             $this->initSession($paramsSession);
+            if (!empty($persistantObjects)) {
+                $this->initPersistantObjs($serviceManager, $persistantObjects);
+            }
         }
     }
 
@@ -49,6 +70,9 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
         );
     }
 
+    /**
+     * @return array|void|\Zend\ServiceManager\Config
+     */
     public function getViewHelperConfig()
     {
     }
@@ -63,5 +87,25 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
         $sessionManager = new SessionManager($sessionConfig);
         $sessionManager->start();
         Container::setDefaultManager($sessionManager);
+    }
+
+    /**
+     * @param ServiceManager $sm
+     * @param array $persistantObjects
+     * @throws \Exception
+     */
+    public function initPersistantObjs(ServiceManager $sm, array $persistantObjects)
+    {
+        $container = new Container('gotObjList');
+        $container->persistObjs = serialize($persistantObjects);
+
+        foreach ($persistantObjects as $id => $class) {
+            if (is_subclass_of($class, PersistObjectInterface::class)) {
+                /** @var OObject|PersistObjectInterface $tmpObj */
+                $tmpObj = new $class($id);
+                $tmpObj->init($sm);
+                $tmpObj->saveProperties();
+            }
+        }
     }
 }
