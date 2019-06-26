1ac7e95126885324171dba4b36c178ccf8aec4aa
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2019-06-26 10:02:53 +0200

    ODDragNDrop : recodage de la méthode getData(evt) pour prise en compte de la valeur de evt dans les données retournées

--- public/oobjects/odcontained/oddragndrop/js/oddragndrop.js
+++ public/oobjects/odcontained/oddragndrop/js/oddragndrop.js
@@ -11,9 +11,21 @@ function oddragndrop(obj) {
 
 oddragndrop.prototype = {
 	getData: function (evt) {
-		return {id: this.id, value : null, event : evt, object : this.objet};
+		var val	= '';
+		switch (evt) {
+			case 'upload':
+			case 'delete':
+				val 	= null;
+			default:
+				var eltSelected = $("#"+this.id+" .file-preview .file-preview-thumbnails .file-preview-frame"+
+				" file-thumbnail-footer .file-footer-caption .file-caption-info");
+				$.each(eltSelected, function (i, selected) {
+					val[i]	= selected.text();
+				})
+		}
+		return {id: this.id, value : val, event : evt, object : this.objet};
 	},
 	setData: function (data) {
-
+		// no need according to initialization function of file-input
 	},
 };
